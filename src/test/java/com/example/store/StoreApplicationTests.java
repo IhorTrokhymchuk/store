521ea58ac63316d9ec6ab388c0bd1ed131f366e2
IhorTrokhymchuk <hameleon009@gmail.com>
2024-02-19 14:39:00 +0200

    Revert "Added docker supports"
    This reverts commit 3bed349490fd87eeca7008dcc86383224fb92a9f.

--- src/test/java/com/example/store/StoreApplicationTests.java
+++ src/test/java/com/example/store/StoreApplicationTests.java
@@ -1,9 +1,12 @@
 package com.example.store;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StoreApplicationTests {
 
-
+    @Test
+    void contextLoads() {
+    }
 }
